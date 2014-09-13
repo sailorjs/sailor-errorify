802b5561874432c2b83073ddaa6e82a3fc76ac14
Kiko Beats <josefrancisco.verdu@gmail.com>
2014-09-13 18:47:42 +0200

    updated

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 /**
  * Dependencies
  */
-var WLError           = require ('./lib/WLError').new_WLError;
-var WLValidationError = require ('./lib/WLValidationError');
 var expressValidator  = require('express-validator-errors');
-var _                 = require('lodash');
 
+var errorify = _.assign(expressValidator, {});
 
 /**
  * Exports
  */
-var errorify = _.assign(expressValidator, {
-  custom     : WLError,
-  waterline  : WLValidationError
-});
-
 exports = module.exports = errorify;
